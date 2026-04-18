970f2f77077585698c39ff36fa97bcb462255aa3
kkamangcode <87938309+kkamangcode@users.noreply.github.com>
2026-04-18 10:45:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if st.button("추천 보기"):
         st.write("조건에 맞는 장소가 없습니다")
     else:
         for place in recommendations:
-            st.write("추천장소는",place["이름"])
+            st.write("추천장소는",place["이름"],"입니다.")
             st.write("한줄설명 : ",place["한줄설명"])
-            st.write("필요한 예산은",place["예산"],"원 입니다")
+            st.write("필요한 예산은",place["예산"],"원 입니다.")
             st.write("---")
